1ec73ec648a234e481e579fe87ae414560f561c7
VladimirMurach <vladimirmurach@gmail.com>
2024-05-01 17:57:51 +0300

    Добавлен YamlReader

--- lab3/src/main/java/labs/lab3/JsonReader.java
+++ lab3/src/main/java/labs/lab3/JsonReader.java
@@ -1,13 +1,26 @@
 package labs.lab3;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 class JsonReader {
 
     ArrayList<ReactorType> read(File file) {
-        System.out.println("JSON");
-        return new ArrayList<>();
-    }
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        ArrayList<ReactorType> reactorTypes = new ArrayList<>();
+        try {
+            reactorTypes = objectMapper.readValue(file,
+                    objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, ReactorType.class));
+            for (ReactorType reactorType : reactorTypes) {
+                reactorType.setSource("From JSON");
+            }
+            System.out.println("JSON успешно прочитан");
+        } catch (IOException e) {
+            System.out.println("Ошибка");
+        }
+        return reactorTypes;
+    }
 }
--- lab3/src/main/java/labs/lab3/YamlReader.java
+++ lab3/src/main/java/labs/lab3/YamlReader.java
@@ -1,13 +1,40 @@
 package labs.lab3;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Map;
+import org.yaml.snakeyaml.Yaml;
 
 class YamlReader {
 
     ArrayList<ReactorType> read(File file) {
-        System.out.println("YAML");
-        return new ArrayList<>();
+
+        ArrayList<ReactorType> reactorTypes = new ArrayList<>();
+        try {
+            Yaml yaml = new Yaml();
+            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(file.getName());
+            Iterable<Object> documents = yaml.loadAll(inputStream);
+            for (Object document : documents) {
+                Map<String, Object> obj = (Map<String, Object>) document;
+                ReactorType reactor = new ReactorType();
+                for (String key : obj.keySet()) {
+                    reactor.setType((String) obj.get("type"));
+                    reactor.setBurnup((double) obj.get("burnup"));
+                    reactor.setKpd((double) obj.get("kpd"));
+                    reactor.setEnrichment((double) obj.get("enrichment"));
+                    reactor.setThermalCapacity((int) obj.get("thermalCapacity"));
+                    reactor.setElectricalCapacity((double) obj.get("electricalCapacity"));
+                    reactor.setLifeTime((int) obj.get("lifeTime"));
+                    reactor.setFirstLoad((double) obj.get("firstLoad"));
+                    reactor.setSource("From YAML");
+                }
+                reactorTypes.add(reactor);
+            }
+            System.out.println("YAML успешно прочитан");
+        } catch (org.yaml.snakeyaml.error.YAMLException ex) {
+            System.out.println("Ошибка");
+        }
+        return reactorTypes;
     }
-    
 }
